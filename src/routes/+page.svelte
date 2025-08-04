264df78286bb501b6a75aab9d3d661662a24c62d
navinate <treycluff@gmail.com>
2025-08-03 20:38:32 -0400

    added QR stuff to pages and reworked layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,61 +1,109 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import Button from '$lib/components/ui/button/button.svelte';
+	import { Button, buttonVariants } from '$lib/components/ui/button/index.js';
 	import * as Card from '$lib/components/ui/card/index.js';
 	import * as InputOTP from '$lib/components/ui/input-otp';
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
 	import { gameStore } from '$lib/stores/gameStore';
-	import type { Room } from 'colyseus.js';
+	import ScanQRCode from '@lucide/svelte/icons/scan-qr-code';
+	import QRScanner from '$lib/components/QRScanner.svelte';
+	import { onMount } from 'svelte';
+	import { get } from 'svelte/store';
+	import { placeHolderNames } from '$lib/helpers/constants';
 
+	const currentState = get(gameStore);
+
+	let randomName: string = '';
 	let isConnecting: boolean;
-	let joinCode: string = "";
+	let joinCode: string = '';
+	let name: string = '';
 
 	async function hostRoom() {
-		isConnecting = true;
-		try {
-			const room  = await gameStore.createRoom();
-			//console.log(`Room created with join code: ${room.roomID}`);
-			console.log(room)
-			goto(`lobby`);
-		} catch (err) {
-			console.error(err);
-		} finally {
-			isConnecting = false;
+		if (name) {
+			isConnecting = true;
+			try {
+				const room = await gameStore.createRoom(name);
+
+				//console.log(`Room created with join code: ${room.roomID}`);
+				console.log(room);
+				goto(`/lobby`);
+			} catch (err) {
+				console.error(err);
+			} finally {
+				isConnecting = false;
+			}
+		} else {
+			alert("Please enter a name")
 		}
 	}
 
 	async function joinRoom() {
-		if (!joinCode.trim()) return;
-		isConnecting = true;
-		try {
-			await gameStore.joinByCode(joinCode.trim());
-			goto(`/lobby`);
-		} catch (err) {
-			console.error(err);
-		} finally {
-			isConnecting = false;
+		if (joinCode && /^\d{6}$/.test(joinCode) && name) {
+			isConnecting = true;
+			try {
+				await gameStore.joinByCode(joinCode.trim(), name.trim());
+				goto(`/lobby`);
+			} catch (err) {
+				console.error(err);
+				alert("cannot find room, please try another code")
+			} finally {
+				isConnecting = false;
+			}
+		} else {
+			if (!joinCode) alert('no code entered');
+			else if(!name) alert('please enter a name');
+			else alert("bad code");
 		}
 	}
+
+	onMount(() => {
+		randomName = placeHolderNames[Math.floor(Math.random() * placeHolderNames.length)];
+		if (currentState.room !== null) {
+			gameStore.disconnect();
+		}
+	});
 </script>
 
 <main class="flex min-h-screen items-center justify-center">
 	<Card.Root>
-		<Card.Header>
-			<Card.Title>Debug Index</Card.Title>
-			<Card.Description>Quick navigate to app pages</Card.Description>
+		<Card.Header class="flex justify-between">
+			<Card.Title>Trey's Scavenge Game</Card.Title>
+			<Dialog.Root>
+				<Dialog.Trigger class={buttonVariants({ variant: 'outline' })}>
+					<ScanQRCode />
+				</Dialog.Trigger>
+				<Dialog.Content>
+					<QRScanner
+						onscan={(result) => {
+							joinCode = result;
+							joinRoom();
+						}}
+					/>
+				</Dialog.Content>
+			</Dialog.Root>
 		</Card.Header>
-		<Card.Content>
-			<InputOTP.Root maxlength={9} bind:value={joinCode}>
-				{#snippet children({ cells })}
-					<InputOTP.Group>
-						{#each cells as cell (cell)}
-							<InputOTP.Slot {cell} />
-						{/each}
-					</InputOTP.Group>
-				{/snippet}
-			</InputOTP.Root>
-			<div class="flex justify-around w-full p-6">
-			<Button onclick={joinRoom}>Join Room</Button>
-			<Button onclick={hostRoom}>Host Room</Button>
+		<Card.Content class="flex flex-col gap-4">
+			<div class="flex w-full max-w-sm flex-col gap-1.5">
+				<Label for="nameInput">Name</Label>
+				<Input id="nameInput" type="name" bind:value={name} placeholder={randomName} />
+			</div>
+			<div class="flex w-full max-w-sm flex-col gap-1.5">
+				<Label for="joinCodeInput">Join Code</Label>
+				<InputOTP.Root maxlength={6} id="joinCodeInput" bind:value={joinCode}>
+					{#snippet children({ cells })}
+						<InputOTP.Group>
+							{#each cells as cell (cell)}
+								<InputOTP.Slot {cell} />
+							{/each}
+						</InputOTP.Group>
+					{/snippet}
+				</InputOTP.Root>
+			</div>
+			<div class="flex w-full max-w-sm flex-col gap-1.5">
+				<Button onclick={joinRoom}>Join room</Button>
+				<Button onclick={hostRoom}>Or create a new room</Button>
 			</div>
 		</Card.Content>
 	</Card.Root>
--- src/routes/lobby/+page.svelte
+++ src/routes/lobby/+page.svelte
@@ -1,21 +1,28 @@
 <script lang="ts">
-    import Button from '$lib/components/ui/button/button.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
 	import * as Card from '$lib/components/ui/card/index.js';
 	import { gameStore } from '$lib/stores/gameStore';
-	import type { PageData } from '../[slug]/lobby/$types';
+	import { get } from 'svelte/store';
+	import QRGenerator from '$lib/components/QRGenerator.svelte';
+	const currentState = get(gameStore);
+	const lobbyID = currentState.room?.roomId || '';
 
-    let data: PageData = $props();
+	function startGame() {}
 </script>
 
 <main class="flex min-h-screen items-center justify-center">
 	<Card.Root>
 		<Card.Header>
-			<Card.Title>Lobby</Card.Title>
-			<Card.Description>Waiting for game to start</Card.Description>
+			<Card.Title>Waiting for game to start</Card.Title>
+			<Card.Description>{lobbyID}</Card.Description>
 		</Card.Header>
-		<Card.Content>
-			<Button href="/lobby">Lobby</Button>
-			<Button href="/take-picture">Take Picture</Button>
+		<Card.Content class="flex flex-col gap-1">
+			
+			{#if currentState.isHost}
+				<Button onclick={startGame}>Start Game</Button>
+			{/if}
+			<QRGenerator text={lobbyID} size={250} />
+			<Button href="/">Leave Room</Button>
 		</Card.Content>
 	</Card.Root>
 </main>
